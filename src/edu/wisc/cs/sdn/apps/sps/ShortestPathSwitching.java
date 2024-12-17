8aa66158ab26d919ed4b293e21d730e61f815dfd
yx-xyc <yx2021@nyu.edu>
2024-12-17 11:36:59 -0500

    set logging to be true

--- src/edu/wisc/cs/sdn/apps/sps/ShortestPathSwitching.java
+++ src/edu/wisc/cs/sdn/apps/sps/ShortestPathSwitching.java
@@ -275,13 +275,12 @@ public class ShortestPathSwitching implements IFloodlightModule, IOFSwitchListen
 	public Map<Long, Integer> getShortestPaths(IOFSwitch srcSwitch, Collection<Link> links, Map<Long, IOFSwitch> switches) {
 		int WEIGHT = 1;
 		Map<Long, Integer> distTo = new HashMap<Long, Integer>();	// dst -> distance
-		Map<Long, int[]> pathInfo = new HashMap<Long, int[]>();	// dst -> predecessor's port
+		Map<Long, Integer> edgeTo = new HashMap<Long, Integer>();	// dst -> predecessor's port
 		for (long sId : switches.keySet()) {
-			distTo.put(sId, Integer.MAX_VALUE);
-			pathInfo.put(sId, new int[] {-1, -1});
+			distTo.put(sId, Integer.MAX_VALUE - 1);
 		}
 		distTo.put(srcSwitch.getId(), 0);
-		for (int v = 0; v < switches.size() - 2; v++) {
+		for (int v = 0; v < switches.size(); v++) {
 			// relax
 			for (Link link: links) {
 				long src = link.getSrc(), dst = link.getDst();
